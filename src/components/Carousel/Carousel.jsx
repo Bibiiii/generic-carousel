1648fdfa890f265d2c87b553a648086161aa4b96
Catherine Oxley <catherine.oxley@live.co.uk>
2018-12-08 13:19:06 +0000

    Add basic Carousel files and set up Carousel class

---
+++ src/components/Carousel/Carousel.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Carousel extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+  render() {
+    const { children } = this.props;
+
+    return (
+      <div>
+        { children }
+      </div>
+    )
+  }
+}
+Carousel.propTypes = {
+  children: PropTypes.node
+};
+
+export default Carousel;
